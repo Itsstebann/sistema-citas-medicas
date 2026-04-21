186006cf8dfcdda9881515b4bed18edfa89932a6
Itsstebann <devstack.drodriguez@gmail.com>
2026-04-20 22:22:38 -0500

    feat: habilito CORS para frontend

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import pacientes_router, doctores_router, citas_router, auth_router
 
 app = FastAPI(
     title="Sistema de Citas Médicas",
     description="API REST para gestión de citas médicas",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
